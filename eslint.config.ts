a2e076fdab74b1ee1e487d2b609f213f94f25748
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2025-08-08 13:35:30 +0200

    Add initial implementation of Pi-hole synchronization service
    Includes:
    - Command-line tool setup with `commander`.
    - Configuration parsing via YAML.
    - Synchronization service for Pi-hole allowed domains and local DNS records.
    - Logger setup using winston with custom formatting.
    - TypeScript types for Pi-hole interaction.
    - ESLint and Prettier configurations for code quality.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,22 +1,38 @@
-import * as globals from "globals";
-import * as tseslint from "typescript-eslint";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
-import { defineConfig } from "eslint/config";
+import * as globals from 'globals';
+import * as tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import { defineConfig } from 'eslint/config';
 import * as importPlugin from 'eslint-plugin-import';
-import * as js from "@eslint/js";
+import * as js from '@eslint/js';
 
 export default defineConfig([
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node }},
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.node },
+    },
     tseslint.configs.recommended,
-    { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-    { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-    { ignores: ['package-lock.json']},
+    {
+        files: ['**/*.json'],
+        plugins: { json },
+        language: 'json/json',
+        extends: ['json/recommended'],
+    },
+    {
+        files: ['**/*.md'],
+        plugins: { markdown },
+        language: 'markdown/gfm',
+        extends: ['markdown/recommended'],
+    },
+    { ignores: ['package-lock.json'] },
     importPlugin.flatConfigs.recommended,
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
         rules: {
             'import/order': 'error',
             'import/no-unresolved': 'off',
-        }
+        },
     },
 ]);
--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
 # pihole data sync
 
 I use this script to sync my home lab pi-holes to have the same data:
+
 - allow domains
 - local dns records
 
 How to run it:
+
 ```bash
 cp config.yaml.example config.yaml
 npm run sync -- --config config.yaml
